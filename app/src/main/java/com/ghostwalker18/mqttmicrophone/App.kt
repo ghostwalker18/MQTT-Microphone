37d45eb9ad0f83d729487921bc43c00b576c010f
ghostwalker18 <76010772+ghostwalker18@users.noreply.github.com>
2024-12-10 16:48:10 +0300

    added language and theme support

--- app/src/main/java/com/ghostwalker18/mqttmicrophone/App.kt
+++ app/src/main/java/com/ghostwalker18/mqttmicrophone/App.kt
@@ -15,7 +15,65 @@
 package com.ghostwalker18.mqttmicrophone
 
 import android.app.Application
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.os.LocaleListCompat
 import dagger.hilt.android.HiltAndroidApp
+import java.util.Locale
+import javax.inject.Inject
 
+/**
+ * <h1>MQTT Microphone</h1>
+ * <p>
+ *      Программа представляет собой мобильный микрофон для отправки голосовых записей на MQTT сервер.
+ * </p>
+ *
+ * @author  Ипатов Никита
+ * @version  1.0
+ */
 @HiltAndroidApp
-class App: Application() {}
+class App: Application(), SharedPreferences.OnSharedPreferenceChangeListener {
+    @Inject lateinit var preferences: SharedPreferences
+
+    override fun onCreate() {
+        super.onCreate()
+        val theme = preferences.getString("theme", "")
+        setTheme(theme)
+        preferences.registerOnSharedPreferenceChangeListener(this)
+    }
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+        when(key){
+            "theme" -> {
+                val theme = sharedPreferences?.getString(key, "")
+                setTheme(theme)
+            }
+            "language" -> {
+                val localeCode = sharedPreferences?.getString(key, "en")
+                setLocale(localeCode)
+            }
+        }
+    }
+
+    /**
+     * Этот метод позволяет установить язык приложения
+     * @param localeCode код языка
+     */
+    private fun setLocale(localeCode : String?) {
+        val localeListCompat = if (localeCode == "system")
+            LocaleListCompat.getEmptyLocaleList()
+        else
+            LocaleListCompat.create(localeCode?.let { Locale(it) })
+        AppCompatDelegate.setApplicationLocales(localeListCompat)
+    }
+    /**
+     * Этот метод позволяет установить тему приложения
+     * @param theme код темы (system, day, night)
+     */
+    private fun setTheme(theme : String?){
+        when(theme){
+            "system" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+            "night" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            "day" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+    }
+}
