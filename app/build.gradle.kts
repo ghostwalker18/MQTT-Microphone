29bbe23a4abc73cfd2e061c08b74de3d75ed4bd4
ghostwalker18 <76010772+ghostwalker18@users.noreply.github.com>
2024-12-10 13:23:41 +0300

    added hilt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.kapt")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
@@ -39,15 +41,21 @@ android {
     buildFeatures {
         viewBinding = true
     }
+    kapt {
+        correctErrorTypes = true
+    }
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.preference:preference-ktx:1.2.1")
+    implementation("org.eclipse.paho:org.eclipse.paho.client.mqttv3:1.1.0")
+    implementation("org.eclipse.paho:org.eclipse.paho.android.service:1.1.1")
+    implementation("com.google.dagger:hilt-android:2.51")
+    kapt("com.google.dagger:hilt-compiler:2.51")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
--- app/src/main/java/com/ghostwalker18/mqttmicrophone/MainActivity.kt
+++ app/src/main/java/com/ghostwalker18/mqttmicrophone/MainActivity.kt
@@ -14,12 +14,17 @@
 
 package com.ghostwalker18.mqttmicrophone
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.MotionEvent
 import com.ghostwalker18.mqttmicrophone.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 /**
  * Этот класс представляет собой основной экран приложения.
@@ -27,14 +32,42 @@ import com.ghostwalker18.mqttmicrophone.databinding.ActivityMainBinding
  * @author  Ипатов Никита
  * @since 1.0
  */
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var notStarted = false
+    @Inject lateinit var recorder: VoiceRecorder
+    @Inject lateinit var prefs: SharedPreferences
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
+        when(prefs.getString("record_mode", "hold")){
+            "hold" -> {
+                binding.mic.setOnTouchListener { _, motionEvent ->
+                    when(motionEvent.action){
+                        MotionEvent.ACTION_DOWN -> startRecord()
+                        MotionEvent.ACTION_UP -> sendRecord()
+                    }
+                    true
+                }
+            }
+            "tap" -> {
+                binding.mic.setOnClickListener {
+                    if(notStarted){
+                        startRecord()
+                        notStarted = !notStarted
+                    } else {
+                        sendRecord()
+                    }
+                }
+            }
+
+        }
+        binding.mic.setOnClickListener { sendRecord() }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -51,5 +84,13 @@ class MainActivity : AppCompatActivity() {
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun startRecord() {
+
+    }
+
+    private fun sendRecord(){
+
     }
 }
